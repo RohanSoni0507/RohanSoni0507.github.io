61f0ddd81dc821e76da7d63adf85ceb0436bbcbe
Rohan Soni <rohansoni342ni@gmail.com>
2023-10-10 08:06:01 +0530

    update

--- AIDesk/server.js
+++ AIDesk/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, '')));
 
 app.listen(PORT, () => {

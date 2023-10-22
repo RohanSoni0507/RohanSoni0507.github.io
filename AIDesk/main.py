98575a320f5066381e9e2d400195e5dc0de773c0
Rohan Soni <rohansoni342ni@gmail.com>
2023-10-22 11:19:23 +0530

    up

--- AIDesk/main.py
+++ AIDesk/main.py
@@ -1,8 +1,20 @@
 import webbrowser
+import datetime
+import speech_recognition as sr
+import requests
+import json
 
 def main():
-    while True:
-        command = input("Enter a voice command: ").lower()
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        command = recognizer.recognize_google(audio).lower()
+        print(f"You said: {command}")
 
         if "open youtube" in command:
             webbrowser.open("https://www.youtube.com/")
@@ -10,12 +22,61 @@ def main():
             webbrowser.open("https://www.google.com/")
         elif "open stack overflow" in command:
             webbrowser.open("https://stackoverflow.com/")
-        elif "tell me the time" in command:
-            import datetime
-            current_time = datetime.datetime.now().strftime("%H:%M:%S")
-            print(f"Current time is {current_time}")
+        elif "check weather" in command:
+            get_weather()
+        elif "check news" in command:
+            get_news()
+        elif "calculate" in command:
+            expression = command.replace("calculate", "").strip()
+            evaluate_math_expression(expression)
         else:
             print("Command not recognized.")
 
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio.")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+
+def get_weather():
+    city = "Chengalpattu"
+    api_key = "YOUR_OPENWEATHERMAP_API_KEY"
+    weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+    response = requests.get(weather_url)
+    data = response.json()
+
+    if data["cod"] == 200:
+        weather_description = data["weather"][0]["description"]
+        temperature = round(data["main"]["temp"] - 273.15, 2)  # Convert temperature to Celsius
+        print(f"The weather in {city} is currently {weather_description} with a temperature of {temperature}°C.")
+    else:
+        print("Unable to fetch weather information. Please try again.")
+
+def get_news():
+    country = "in"
+    api_key = "YOUR_NEWSAPI_API_KEY"  # Replace with your News API key
+    news_source = "bbc-news"
+    news_url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}"
+
+    response = requests.get(news_url)
+    data = response.json()
+
+    if data["status"] == "ok" and data["articles"]:
+        news_articles = data["articles"]
+        print("Here are the latest news updates:")
+        for index, article in enumerate(news_articles, start=1):
+            print(f"{index}. {article['title']}")
+    else:
+        print("Unable to fetch news updates. Please try again.")
+
+def evaluate_math_expression(expression):
+    try:
+        result = eval(expression)
+        print(f"Result: {result}")
+    except Exception as e:
+        print(f"Math expression evaluation error: {e}")
+        print("Invalid math expression. Please try again.")
+
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
--- MorseyCode/app.js
+++ MorseyCode/app.js
@@ -85,3 +85,53 @@ document.addEventListener('DOMContentLoaded', function () {
         return words.join(' ');
     }    
 });
+
+const projectsData = [
+    {
+        name: "Library Management System",
+        image: "project1.jpg",
+        website: "https://rohansoni0507.github.io/BookWise/",
+    },
+    {
+        name: "Desktop Voice Assistant",
+        image: "project2.jpg",
+        website: "https://rohansoni0507.github.io/AIDesk/",
+    },
+    {
+        name: "Round Robin Web Scheduler",
+        image: "project3.jpg",
+        website: "https://rohansoni0507.github.io/RRSim/",
+    },
+    {
+        name: "Productivity Tracker",
+        image: "project4.jpg",
+        website: "https://rohansoni0507.github.io/ProductivityPro/",
+    },
+];
+
+function createProjectCards() {
+    const projectContainer = document.querySelector(".project-card-container");
+
+    projectsData.forEach((project) => {
+        const card = document.createElement("div");
+        card.classList.add("project-card");
+
+        const image = document.createElement("img");
+        image.src = project.image;
+        image.alt = project.name;
+
+        const projectName = document.createElement("h3");
+        projectName.textContent = project.name;
+
+        const projectLink = document.createElement("a");
+        projectLink.href = project.website;
+        projectLink.textContent = "Visit Project";
+
+        card.appendChild(image);
+        card.appendChild(projectName);
+        card.appendChild(projectLink);
+
+        projectContainer.appendChild(card);
+    });
+}
+createProjectCards();
